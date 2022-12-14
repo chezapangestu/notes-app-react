3b6258ed40ec9a1a79002b2b08f40fdec5f4b230
chezapangestu <cheza426@gmail.com>
2022-12-14 14:01:40 +0700

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import Header from "./Header";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+
+function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes((prevNotes) => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    // console.log("Was deleetdd");
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteitem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      <div className="middle">
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      </div>
+      {/* <Note key={1} title="Note title" content="Note content" /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="theNavbar">
+      <h1>Note's App</h1>
+    </header>
+  );
+}
+
+export default Header;
